4bbfd8ac8ab680791eaea72e21f61b62e3d90410
GeoPleaser <mrenvij@gmail.com>
2025-02-24 01:28:05 +0200

    commit 5

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-import math\ndef factorial(n): return math.factorial(n)
+from utils import factorial
+
+result = factorial(5)
+print(f"Факторіал числа 5 дорівнює: {result}")
+
+from utils import is_prime
+
+print(is_prime(17)) 
+
+from utils import is_power_of_five
+
+numbers = [1, 5, 25, 125, 7, 10, 625]
+for num in numbers:
+    if is_power_of_five(num):
+        print(f"{num} є степенем 5.")
+    else:
+        print(f"{num} не є степенем 5.")
+
+from utils import is_power_of_two
+
+numbers = [1, 2, 4, 8, 16, 3, 5, 10, 32]  # 1 = 2^0, 2 = 2^1, 4 = 2^2, 8 = 2^3, 16 = 2^4, 32 = 2^5
+for num in numbers:
+    if is_power_of_two(num):
+        print(f"{num} є степенем 2.")
+    else:
+        print(f"{num} не є степенем 2.")
--- utils.py
+++ utils.py
@@ -1,1 +1,38 @@
-import math\ndef factorial(n): return math.factorial(n)
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+def is_power_of_five(n):
+    if n <= 0:
+        return False
+    if n == 1:
+        return True
+
+    while n > 1:
+        if n % 5 != 0:
+            return False
+        n //= 5
+    return True
+
+def is_power_of_two(n):
+    if n <= 0:
+        return False
+    if n == 1:
+        return True
+
+    while n > 1:
+        if n % 2 != 0:
+            return Fals
+        n //= 2
+    return True
+
